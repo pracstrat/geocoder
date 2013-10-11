cf002460c49753514f0feb067c4275679fb08d18
programmer <peter@51shepherd.com>
2013-10-11 11:17:53 +0800

    define the class location into moduel

--- lib/geocoder/lookups/location.rb
+++ lib/geocoder/lookups/location.rb
@@ -1,6 +1,10 @@
-class Location
-  attr_accessor :street, :city, :state, :zipcode, :lng, :lat
-  def initialize(options={})
-    options.each{|k,v| instance_variable_set("@#{k.to_s}", v)}
+module Geocoder
+  module Lookup
+    class Location
+      attr_accessor :street, :city, :state, :zipcode, :lng, :lat
+      def initialize(options={})
+        options.each{|k,v| instance_variable_set("@#{k.to_s}", v)}
+      end
+    end
   end
 end
